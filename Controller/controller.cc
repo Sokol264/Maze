10695a36fc8f7f00b65680170e131c58292ed83b
Merlyn Tickler <mtickler@si-c6.nsk.21-school.ru>
2022-08-05 19:51:06 +0700

    feat: add controller

---
+++ Controller/controller.cc
@@ -0,0 +1,37 @@
+#include "controller.h"
+
+using s21::Controller;
+
+Controller::Controller() : facade_(std::make_unique<Facade>()) {}
+
+void Controller::SetHeight(int value) {
+    facade_->SetHeight(value);
+}
+
+void Controller::SetWidth(int value) {
+    facade_->SetWidth(value);
+}
+
+void Controller::GenerateMaze() {
+    facade_->GenerateMaze();
+}
+
+void Controller::ReadLabyrinthFromFile(const std::string &fileName) {
+    facade_->ReadLabyrinthFromFile(fileName);
+}
+
+void Controller::SearchWay() {
+    facade_->SearchWay();
+}
+
+int Controller::width() {
+    return facade_->width();
+}
+
+int Controller::height() {
+    return facade_->height();
+}
+
+typename Controller::TripleVector Controller::walls() {
+    return facade_->walls();
+}
---
+++ Controller/controller.h
@@ -0,0 +1,24 @@
+#ifndef CONTROLLER_H_
+#define CONTROLLER_H_
+
+namespace s21 { class Controller; }
+#include "../facade.h"
+
+class s21::Controller {
+    using TripleVector = std::vector<std::vector<std::vector<int>>>;
+ public:
+    Controller();
+    void SetHeight(int value);
+    void SetWidth(int value);
+    void GenerateMaze();
+    void SearchWay();
+    void ReadLabyrinthFromFile(const std::string &fileName);
+    // Getters
+    int width();
+    int height();
+    TripleVector walls();
+ private:
+    std::unique_ptr<Facade> facade_;
+};
+
+#endif  // CONTROLLER_H_
